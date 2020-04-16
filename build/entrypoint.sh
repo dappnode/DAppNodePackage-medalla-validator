514590e47cc6af049b6781a66b1df4e46e06f7f0
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-04-16 20:25:19 +0200

    Added self-signed certifictes

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -8,8 +8,8 @@ DIR=/data
 
 mkdir -p ${DIR}
 
-ls -lrt $DIR
 while [ -z "$(ls -A -- "$DIR")" ]; do
+    echo "Generating a new accounts..."
     validator accounts create --keystore-path=/data --password=${PASSWORD} 2>/dev/null >/data/deposit_data.txt || rm /data/*
     mv /data/validator* /data/validatorprivatekey
     mv /data/shardwithdrawal* /data/shardwithdrawalkey
@@ -17,13 +17,14 @@ done
 
 if [ ! -f /data/password ]; then
     echo $PASSWORD > /data/password
-elif [ "$PASSWORD" != '' ] && [ "$PASSWORD" != $(cat /data/password) ]; then
+elif [[ $PASSWORD != "" ]] && [[ $PASSWORD != $(cat /data/password) ]]; then
     echo $PASSWORD > /data/password
-elif [ "$PASSWORD" == '' ]; then
+elif [ $PASSWORD == "" ]; then
     export PASSWORD=$(cat /data/password)
 fi
 
 exec validator \
+    --tls-cert=/ssl/ssl.crt \
     --beacon-rpc-provider=${RPC_PROVIDER} \
     --verbosity=${VERBOSITY} \
     --keymanager=keystore \
