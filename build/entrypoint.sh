d4a948b7b117c1e900bb8c4567d9b0fd9fa80233
Eduardo Antuña Díez <eduadiez@gmail.com>
2020-05-04 11:19:33 +0200

    Add ethdo binary

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -2,32 +2,35 @@
 
 RPC_PROVIDER=prysm-beacon-chain.public.dappnode:4000
 LOG=/var/log/validator.log
+KEYMANAGER=${KEYMANAGER:-"keystore"}
 PASSWORD=${PASSWORD:-}
 KEYSTORE=/data
 DIR=/data
 
 mkdir -p ${DIR}
 
-while [ -z "$(ls -A -- "$DIR")" ]; do
-    echo "Generating a new accounts..."
-    validator accounts create --keystore-path=/data --password=${PASSWORD} 2>/dev/null >/data/deposit_data.txt || rm /data/*
-    mv /data/validator* /data/validatorprivatekey
-    mv /data/shardwithdrawal* /data/shardwithdrawalkey
-done
-
-if [ ! -f /data/password ]; then
+if [[ $KEYMANAGER == "keystore" ]]; then
+    while [ -z "$(ls -A -- "$DIR")" ]; do
+        echo "Generating a new accounts..."
+        validator accounts create --keystore-path=/data --password=${PASSWORD} #2>/dev/null >/data/deposit_data.txt || rm /data/*
+        mv /data/validator* /data/validatorprivatekey
+        mv /data/shardwithdrawal* /data/shardwithdrawalkey
+    done
+    if [ ! -f /data/password ]; then
     echo $PASSWORD > /data/password
-elif [[ $PASSWORD != "" ]] && [[ $PASSWORD != $(cat /data/password) ]]; then
-    echo $PASSWORD > /data/password
-elif [ $PASSWORD == "" ]; then
-    export PASSWORD=$(cat /data/password)
+    elif [[ $PASSWORD != "" ]] && [[ $PASSWORD != $(cat /data/password) ]]; then
+        echo $PASSWORD > /data/password
+    elif [ $PASSWORD == "" ]; then
+        export PASSWORD=$(cat /data/password)
+    fi
+    KEYMANAGEROPTS='{"path":"'${KEYSTORE}'","passphrase":"'${PASSWORD}'"}'
 fi
 
 exec validator \
     --tls-cert=/ssl/ssl.crt \
     --beacon-rpc-provider=${RPC_PROVIDER} \
     --verbosity=${VERBOSITY} \
-    --keymanager=keystore \
-    --keymanageropts='{"path":"'${KEYSTORE}'","passphrase":"'${PASSWORD}'"}' \
+    --keymanager=${KEYMANAGER} \
+    --keymanageropts=${KEYMANAGEROPTS} \
     --log-file=${LOG} \
     ${EXTRA_OPTS}
