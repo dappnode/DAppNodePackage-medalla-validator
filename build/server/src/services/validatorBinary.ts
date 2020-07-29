5636fcd6d10cd4fabf8ac6431a9bf5221b419bcc
dapplion <guillem.lion.benosa@gmail.com>
2020-07-29 10:55:43 +0200

    Add lighthouse supervisor process

--- build/server/src/services/validatorBinary.ts
+++ build/server/src/services/validatorBinary.ts
@@ -10,11 +10,14 @@ import {
   extraOpts
 } from "../params";
 
-export const validatorBinary = Supervisor(
+const lighthouseDataDir = "/lighthouse";
+const lighthouseSecretsDir = "/lighthouse/secrets";
+
+export const prysmBinary = Supervisor(
   "validator",
   dargs({
     // "tls-cert": tlsCert,
-    "altona": true,
+    altona: true,
     "monitoring-host": "0.0.0.0",
     "beacon-rpc-provider": beaconRpcProvider,
     keymanager: "wallet",
@@ -23,5 +26,17 @@ export const validatorBinary = Supervisor(
     "log-file": logFile,
     ...(graffiti ? { graffiti } : {}), // Ignore if empty
     _: [extraOpts]
+  })
+);
+
+export const lighthouseBinary = Supervisor(
+  "lighthouse validator_client",
+  dargs({
+    altona: true,
+    "auto-register": true,
+    "strict-lockfiles": true,
+    datadir: lighthouseDataDir,
+    "secrets-dir": lighthouseSecretsDir,
+    server: beaconRpcProvider
   })
 );
