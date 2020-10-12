8658bea98aacdbaf7f5d7a2ddaf730ac083b2531
dapplion <guillem.lion.benosa@gmail.com>
2020-10-12 10:53:55 +0200

    Update Lighthouse API

--- build/server/src/services/beaconNode/interface.ts
+++ build/server/src/services/beaconNode/interface.ts
@@ -1,13 +1,8 @@
-import {
-  ValidatorStatus,
-  BeaconNodeChainhead,
-  SyncStatus
-} from "../../../common";
+import { ValidatorStatus, SyncStatus } from "../../../common";
 
 export type ValidatorStatusByPubkey = { [pubKey: string]: ValidatorStatus };
 
 export interface BeaconNodeClient {
-  chainhead(): Promise<BeaconNodeChainhead>;
   peers(): Promise<string[]>;
   syncing(): Promise<SyncStatus>;
   validators(pubKeys: string[]): Promise<ValidatorStatusByPubkey>;
